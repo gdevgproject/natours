b5b0f213a447a2a57e20d5dff2a08e4f86536818
gdevgproject <gdev.gproject@gmail.com>
2024-12-10 22:53:15 +0700

    v0.1.0 feat: Handling GET tours requests

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
+import fs from 'fs';
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = (relativePath) =>
+  path.join(path.dirname(fileURLToPath(import.meta.url)), relativePath);
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+const tours = JSON.parse(
+  fs.readFileSync(__dirname('/dev-data/data/tours-simple.json'))
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
-});
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port http://127.0.0.1:${port}/`);
