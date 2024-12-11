c359513a124c951f64bc335f9940a487c322c0dd
gdevgproject <gdev.gproject@gmail.com>
2024-12-11 21:24:53 +0700

    v0.2.0 feat: Handling POST a tour requests

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import express from 'express';
+import express, { application } from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
@@ -8,10 +8,15 @@ const __dirname = (relativePath) =>
 
 const app = express();
 
+// Middleware phân tích cú pháp body của request có định dạng JSON, chuyển đổi nó thành đối tượng JavaScript và đưa đối tượng JavaScript đó vào req.body
+app.use(express.json());
+
+// Đọc file data
 const tours = JSON.parse(
   fs.readFileSync(__dirname('/dev-data/data/tours-simple.json'))
 );
 
+// Lấy tất cả tours
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -20,6 +25,26 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+// Thêm một tours với id lớn hơn id của tours cuối cùng
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    __dirname('/dev-data/data/tours-simple.json'),
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
